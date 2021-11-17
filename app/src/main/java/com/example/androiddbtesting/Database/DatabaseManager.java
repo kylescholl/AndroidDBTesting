346672c1634088d0e183f8f11038857fcf823751
kylescholl <kyle.b.scholl@gmail.com>
2021-11-16 23:01:25 -0500

    Adding files

---
+++ app/src/main/java/com/example/androiddbtesting/Database/DatabaseManager.java
@@ -0,0 +1,85 @@
+package com.example.androiddbtesting.Database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.*;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+
+public class DatabaseManager {
+
+    // GLOBAL VARIABLES
+    SQLiteDatabase db = null;
+    String db_name = "";
+    String table_name = "";
+
+    public void createDatabase(Context context){
+        Log.i("Method Call", "createDatabase()");
+
+        db = context.openOrCreateDatabase("LocationDB", context.MODE_PRIVATE, null);
+
+        db.execSQL("Drop table if exists LocationTable;");
+        db.execSQL("Create table if not exists LocationTable (RSSI INT(3), COMPASS_HEADING VARCHAR);");
+
+        db.close();
+    }
+
+    public void logInfo(Context context, int RSSI_VAL, String COMPASS_HEADING) {
+        Log.i("Method Call", "logInfo()");
+
+        db = context.openOrCreateDatabase("LocationDB", context.MODE_PRIVATE, null);
+
+        String query = String.format("Insert into LocationTable VALUES (%s, '%s');", RSSI_VAL, COMPASS_HEADING);
+        db.execSQL(query);
+
+        db.close();
+    }
+
+    public void readDatabase(Context context) {
+        Log.i("Method Call", "logInfo()");
+        db = context.openOrCreateDatabase("LocationDB", context.MODE_PRIVATE, null);
+
+        // on below line we are creating a cursor with query to read data from database.
+        Cursor c = db.rawQuery("Select * FROM LocationTable;", null);
+
+        // on below line we are creating a new array list.
+        ArrayList<Object> db_list = new ArrayList<>();
+
+        Log.i("!!!!!!!!!!!!!!!!!!!!!!", "!!!!!!!!!!!!!!!!!!!!1");
+
+        c.moveToFirst();
+        while(!c.isAfterLast()) {
+            db_list.add(c.getString(c.getColumnIndexOrThrow("COMPASS_HEADING"))); //add the item
+            c.moveToNext();
+        }
+
+        // moving our cursor to first position.
+        if (c.moveToFirst()) {
+            do {
+                Log.i("Cursor Info", c.toString());
+                db_list.add(c.toString());
+            } while (c.moveToNext());
+            // moving our cursor to next.
+        }
+        // at last closing our cursor
+        // and returning our array list.
+        c.close();
+
+        Log.i("!!!!!!!!!!!!!!!!!!!!!!", "!!!!!!!!!!!!!!!!!!!!1");
+
+//        Log.i("!!!!!!!", db_list.size());
+//
+//        ContentValues cv = new ContentValues();
+//        cv.put(FeedEntry.COLUMN_NAME.);
+
+        Cursor c2 = db.rawQuery("Select * FROM LocationTable;", null);
+        c2.moveToFirst();
+        Log.d("COMPASS_HEADING", c2.getString(c2.getColumnIndexOrThrow("COMPASS_HEADING")));
+
+        db.close();
+    }
+}
+
--- app/src/main/java/com/example/androiddbtesting/MainActivity.java
+++ app/src/main/java/com/example/androiddbtesting/MainActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.androiddbtesting.Database.DatabaseManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Instantiate the DatabaseManager class
+        DatabaseManager databaseManager = new DatabaseManager();
+        // Create the database if not already
+        databaseManager.createDatabase(getApplicationContext());
+        // Log location info to database
+        databaseManager.logInfo(getApplicationContext(), -50, "North");
+
+        databaseManager.logInfo(getApplicationContext(), -50, "North");
+        databaseManager.logInfo(getApplicationContext(), -30, "East");
+        databaseManager.logInfo(getApplicationContext(), 20, "South");
+
+        // Read from database
+        databaseManager.readDatabase(getApplicationContext());
     }
 }
