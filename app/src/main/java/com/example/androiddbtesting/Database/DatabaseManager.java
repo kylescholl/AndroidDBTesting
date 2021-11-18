f5ea06c03048dd1c73627a4ac2dd96bd04ed2200
Rushi <rfp5221@psu.edu>
2021-11-17 19:38:43 -0500

    with time

--- app/src/main/java/com/example/androiddbtesting/Database/DatabaseManager.java
+++ app/src/main/java/com/example/androiddbtesting/Database/DatabaseManager.java
@@ -1,13 +1,16 @@
 package com.example.androiddbtesting.Database;
 
-import android.content.ContentValues;
+
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.*;
 import android.util.Log;
 
+
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Calendar;
+import java.util.Date;
+
 
 
 public class DatabaseManager {
@@ -23,17 +26,17 @@ public class DatabaseManager {
         db = context.openOrCreateDatabase("LocationDB", context.MODE_PRIVATE, null);
 
         db.execSQL("DROP TABLE IF EXISTS LocationTable;");
-        db.execSQL("CREATE TABLE IF NOT EXISTS LocationTable (ITEM_NAME VARCHAR, COMPASS_HEADING VARCHAR, RSSI INT(3));");
+        db.execSQL("CREATE TABLE IF NOT EXISTS LocationTable (ITEM_NAME VARCHAR, COMPASS_HEADING VARCHAR, RSSI INT(3), TIME time);");
 
         db.close();
     }
 
-    public void logInfo(Context context, String ITEM_NAME, String COMPASS_HEADING, int RSSI) {
+    public void logInfo(Context context, String ITEM_NAME, String COMPASS_HEADING, int RSSI, int TIME) {
         Log.i("Method Call", "logInfo()");
 
         db = context.openOrCreateDatabase("LocationDB", context.MODE_PRIVATE, null);
 
-        String query = String.format("INSERT INTO LocationTable VALUES ('%s', '%s', %s);", ITEM_NAME, COMPASS_HEADING, RSSI);
+        String query = String.format("INSERT INTO LocationTable VALUES ('%s', '%s', %s, %s);", ITEM_NAME, COMPASS_HEADING, RSSI, TIME);
         db.execSQL(query);
 
         db.close();
@@ -48,12 +51,14 @@ public class DatabaseManager {
         ArrayList<String> name_list = new ArrayList<>();
         ArrayList<String> heading_list = new ArrayList<>();
         ArrayList<String> rssi_list = new ArrayList<>();
+        ArrayList<Date> time_list = new ArrayList<>();
 
         c.moveToFirst();
         while(!c.isAfterLast()) {
             String name = c.getString(c.getColumnIndexOrThrow("ITEM_NAME"));
             String heading = c.getString(c.getColumnIndexOrThrow("COMPASS_HEADING"));
             String rssi = c.getString(c.getColumnIndexOrThrow("RSSI"));
+            Date time = Calendar.getInstance().getTime();
 
             /*
             Log.i("name", name);
@@ -64,6 +69,7 @@ public class DatabaseManager {
             name_list.add(name);
             heading_list.add(heading);
             rssi_list.add(rssi);
+            time_list.add(time);
 
             c.moveToNext();
         }
@@ -76,8 +82,9 @@ public class DatabaseManager {
             String n = name_list.get(i);
             String h = heading_list.get(i);
             String r = rssi_list.get(i);
+            Date t = time_list.get(i);
 
-            out = String.format("['%s', '%s', %s]", n, h, r);
+            out = String.format("['%s', '%s', %s, %s]", n, h, r, t);
             Log.i("row", out);
         }
     }
